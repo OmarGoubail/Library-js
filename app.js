9d26e64444bc950731e196e100b2956eeb01b473
OmarGoubail <omarsayed223@gmail.com>
2022-04-26 00:35:19 +0200

    built function again, have working lib

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-
-let library = [];
-
+let myLibrary = [];
 
 
-function Book(title, author, pages, readingStatus) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.readingStatus = readingStatus
+
+form_div = document.querySelector('.book_form')
+form = document.getElementById('form');
+submit_btn = document.getElementById('submit_book_btn');
+Add_btn = document.getElementById('add_book_btn');
+close_btn = document.getElementById('close_form_btn')
+
+
+
+function Book(title, author, pages, reading_status) {
+  // the constructor...
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.reading_status = reading_status;
+}
+
+
+const Book1 = new Book('War and Peace', 'Tolstoy, Leo', 967, 'Read')
+
+myLibrary.push(Book1)
+
+
+
+function addBookToLibrary() {
+  // do stuff here
+    let formData = new FormData(form)
+
+    let title = formData.get('title')
+    let author = formData.get('author')
+    let pages = formData.get('pages')
+    let reading_status = formData.get('reading_status') == "read" ? 'Read' : "Not read yet" 
+
+    myLibrary.push(new Book("Hard Times", "Dickens, Charles", '460', 'Not read yet'))
+    // console.log(myLibrary)
+
+
 
 }
 
@@ -15,39 +45,17 @@ function Book(title, author, pages, readingStatus) {
 
 
 
-function addBookToLibrary(title, author, pages, readingStatus){
-    
-    book = new Book(title, author, pages, readingStatus)
-    library.push(book)
-    
-    
-}
-
-addBookToLibrary("life", 'omar', 300,'read')
-addBookToLibrary("death", 'Joy', 800,'not yet read')
-addBookToLibrary("Neither alive nor dead", "farida", 100, 'read')
 
 
-function displayBooks(){
+//add Book to Library
+submit_btn.addEventListener("click", addBookToLibrary)
 
-    for (i = 0; i < library.length; i++){
-        
-        console.table(library[i])
-    }
-}
+// Show/Hide form
+Add_btn.addEventListener("click", () => {
+  form_div.classList.toggle("show_book_form");
+});
 
-displayBooks()
+close_btn.addEventListener("click", () => {
+  form_div.classList.toggle("show_book_form");
+});
 
-console.table(library)
-
-const getBookFromInput = () => {
-    const title = document.getElementById('title').value
-    const author = document.getElementById('author').value
-    const pages = document.getElementById('pages').value
-    const isRead = document.getElementById('isRead').checked
-    return addBookToLibrary(title, author, pages, isRead)
-  }
-
-
-getBookFromInput()
-console.table(library)
